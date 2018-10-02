91e1038c301587363a0e5096a24743b07707e668
Renee Wysocki <reneew311@gmail.com>
2018-10-01 20:45:08 -0500

    weather api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
 //set up google maps api 
 
 //set up openweather api 
+var weatherAPIkey = "63ad6cfdee5ea624323fed889a2d525d";
+var currentLocation = "Dallas,Texas"
+var weatherqueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentLocation + "&units=imperial&appid=" + weatherAPIkey;
+
+
+$.ajax({
+  url: weatherqueryURL,
+  method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
 
 //set up trails api 
 
@@ -13,4 +24,7 @@ var config = {
     storageBucket: "",
     messagingSenderId: "1061119694550"
   };
+
   firebase.initializeApp(config);
+
+  var database = firebase.database();
