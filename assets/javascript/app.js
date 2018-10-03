040941df1edfd0e4218ef7bac32841dc06cc6f97
Renee Wysocki <reneew311@gmail.com>
2018-10-03 11:10:30 -0500

    geolocates user and pulls weather from that location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,9 @@
-var longitute = -96.7844135; 
-var lattitude = 32.8448345;
+var latitude = "";
+//-96.7844135; 
+var longitude = "";
+//32.8448345;
 
-//set up openweather api 
 var weatherAPIkey = "63ad6cfdee5ea624323fed889a2d525d";
-//var currentLocation = "Dallas,Texas"
-var weatherqueryURL = "http://api.openweathermap.org/data/2.5/find?lat=" + lattitude + "&lon=" + longitute +"&cnt=1&appid=" + weatherAPIkey;
-
-
-$.ajax({
-  url: weatherqueryURL,
-  method: "GET"
-}).then(function(response) {
-
-    console.log(response);
-});
 
 //set up trails api 
 
@@ -32,21 +22,63 @@ var config = {
   var database = firebase.database();
 
   //set up google maps api 
-  function initMap() {
-    var myLatLng = {lat: lattitude, lng: longitute};
+   // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:  32.8448345, lng: -96.7844135},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-    // Create a map object and specify the DOM element
-    // for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 4
-    });
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
+            //set up openweather api 
+            var weatherqueryURL = "http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude +"&cnt=1&appid=" + weatherAPIkey;
+            $.ajax({
+              url: weatherqueryURL,
+              method: "GET"
+            }).then(function(response) {
+                console.log(response);
+            });
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
 
-    // Create a marker and set its position.
-     var marker = new google.maps.Marker({
-       map: map,
-       position: myLatLng,
-       title: 'Activities Are Here!'
-     });
-  }
- 
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+
+
+
+// $.ajax({
+//   url: weatherqueryURL,
+//   method: "GET"
+// }).then(function(response) {
+//     console.log("Weather: " + response);
+// });
