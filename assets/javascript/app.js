7651d20781d929e3a02cbe687e69d8634f3c832a
JTerry <jessica.terry79@yahoo.com>
2018-10-03 19:28:37 -0400

    added the trailsAPI and tested using longitude and latitude of Dallas, Texas

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,13 @@
+// Global variables
+var trails; // a variable to hold trails
+var places; // a variable to hold places
+
+
 //set up google maps api 
 
 //set up openweather api 
 var weatherAPIkey = "63ad6cfdee5ea624323fed889a2d525d";
-var currentLocation = "Dallas,Texas"
+var currentLocation = "Dallas,Texas";
 var weatherqueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentLocation + "&units=imperial&appid=" + weatherAPIkey;
 
 
@@ -13,7 +18,20 @@ $.ajax({
     console.log(response);
 });
 
-//set up trails api 
+// set up trails api
+var trailsqueryURL = 'https://trailapi-trailapi.p.mashape.com/trails/explore/' + '?lat=37.28225%2C&lon=-107.877762&per_page=10&radius=25';
+     
+      $.ajax({
+        url: trailsqueryURL,
+        method: "GET",
+        headers: {
+          "X-Mashape-Key" :"dZJGfLx5hNmshNppywXnsDamxgDPp1RzSf2jsnYe48JNSRCtXc",
+          "Accept": "application/json"
+        
+      } 
+      }).then(function(response) {
+          console.log(response);
+      });
 
 //initialize firebase
 var config = {
